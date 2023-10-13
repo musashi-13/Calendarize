48e786de06a89bde159826f518d8705cafb570c1
shadowfalcon16 <karanhathwar@gmail.com>
2023-10-13 19:04:25 +0530

    Added fontawesome

--- calendarize/src/App.js
+++ calendarize/src/App.js
@@ -2,14 +2,14 @@ import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
 import './App.css';
 import ReactDOM from 'react-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faUser, faCirclePlus } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
     <navigator className="navBar">
       <h1 className="nameLogo">CALENDARIZE</h1>
-      <button className="addEvent">Add Event</button>
-      <button className="profile"><FontAwesomeIcon icon={faUser} /></button>
+      <button className="addEvent"><FontAwesomeIcon icon={faCirclePlus} size="xl" /></button>
+      <button className="profile"><FontAwesomeIcon icon={faUser} size="xl"/></button>
     </navigator>
   );
 }
