a0e1b2f7fc968da9f343abeab974a68a6f90ee94
shadowfalcon16 <karanhathwar@gmail.com>
2023-10-15 04:05:31 +0530

    Added status bar

--- calendarize/src/App.js
+++ calendarize/src/App.js
@@ -3,12 +3,18 @@ import React, { useState } from 'react';
 import TodayCardContainer from './TodayContainer';
 import UpcomingCardContainer from './UpcomingContainer';
 import Calendar from './calendar';
+import Sidebar from './sideBar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart, faBell } from '@fortawesome/free-solid-svg-icons'
 
 
 function App() {
   const [showLiked, setShowLiked] = useState(false);
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
 
   const toggleShowLiked = () => {
     setShowLiked(!showLiked);
@@ -19,8 +25,9 @@ function App() {
       <nav className="navBar">
         <h1 className="nameLogo">SYNCD</h1>
         <button className="likedEvents" onClick={toggleShowLiked}><FontAwesomeIcon icon={faHeart} size="xl" /></button>
-        <button className="notifications"><FontAwesomeIcon icon={faBell} size="xl"/></button>      
+        <button className="notifications" onClick={toggleSidebar}><FontAwesomeIcon icon={faBell} size="xl"/></button>      
       </nav>
+      <Sidebar isOpen={isSidebarOpen} onClose={toggleSidebar} />
       <h1 className="todayEvents">Today's Events</h1>
       <TodayCardContainer showLiked={showLiked}/>
       <h1 className="upcomingEvents">Upcoming Events</h1>
--- calendarize/src/sideBar.js
+++ calendarize/src/sideBar.js
@@ -1,9 +1,60 @@
-import React from 'react';
+import { faX } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import SidebarCard from './SidebarCard';
+import CollegeEvents from './CollegeEvents.json';
+import cardTheme from './CardTheme';
+
 
 const Sidebar = ({ isOpen, onClose }) => {
   return (
     <div className={`sidebar ${isOpen ? 'open' : ''}`}>
-      <button onClick={onClose}>Close</button>
+      <button className="closeButton" onClick={onClose}><FontAwesomeIcon icon={faX} size='xl'/></button>
+        <div className= "sidebarContainer">
+        <h2 style={{marginLeft: "1em"}}>Hurry up before they close!</h2>
+        {CollegeEvents.map(cEvent => {
+            const eventFromDate = new Date(cEvent.eventFrom);
+            const eventToDate = new Date(cEvent.eventTo);
+            const today = new Date();
+            const closingDate = new Date(cEvent.regStatus);
+            const formattedFromDate = eventFromDate.toLocaleDateString('en-IN', {
+            year: '2-digit',
+            month: '2-digit',
+            day: '2-digit',
+            hour: 'numeric',
+            hour12: true,
+            });
+            const formattedToDate = eventToDate.toLocaleDateString('en-IN', {
+            year: '2-digit',
+            month: '2-digit',
+            day: '2-digit',
+            hour: 'numeric',
+            hour12: true,
+            });
+            const gradientStyle = cardTheme[cEvent.eventTheme];
+            console.log(cEvent.eventName)
+            console.log(closingDate-today)
+            if (
+            closingDate-today < 86400000 && closingDate-today >0
+            ) {
+                return (
+                    <div>
+                    <SidebarCard
+                        EventName={cEvent.eventName}
+                        EventFrom={formattedFromDate}
+                        EventTo={formattedToDate}
+                        EventDesc={cEvent.eventDesc}
+                        StudentCrit={cEvent.studentCriteria}
+                        RegLink={cEvent.regLink}
+                        RegStatus={closingDate - today}
+                        linearGradient={gradientStyle}
+                    />
+                    </div>
+                );
+            } else {
+                return null
+            }
+        })}
+        </div>
     </div>
   );
 };
